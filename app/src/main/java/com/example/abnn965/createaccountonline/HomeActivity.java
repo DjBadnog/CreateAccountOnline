5fbf4474a07099f8e72daa8688281403608a7629
DjBadnog <ntimane.na@gmail.com>
2016-08-01 12:16:47 +0200

    menu items

--- app/src/main/java/com/example/abnn965/createaccountonline/HomeActivity.java
+++ app/src/main/java/com/example/abnn965/createaccountonline/HomeActivity.java
@@ -6,6 +6,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
@@ -38,4 +40,49 @@ public class HomeActivity extends AppCompatActivity {
         startActivity(savingsIntent);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        if (id == R.id.home) {
+            Intent homeIntent = new Intent(HomeActivity.this, HomeActivity.class);
+            startActivity(homeIntent);
+        }
+
+        if (id == R.id.savingsAccounts) {
+            Intent homeIntent = new Intent(HomeActivity.this, HomeActivity.class);
+            startActivity(homeIntent);
+        }
+
+        if (id == R.id.transactAccount) {
+            Intent homeIntent = new Intent(HomeActivity.this, HomeActivity.class);
+            startActivity(homeIntent);
+        }
+
+        if (id == R.id.creditAccount) {
+            Intent homeIntent = new Intent(HomeActivity.this, HomeActivity.class);
+            startActivity(homeIntent);
+        }
+        if (id == R.id.chequeAccount) {
+            Intent homeIntent = new Intent(HomeActivity.this, HomeActivity.class);
+            startActivity(homeIntent);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
